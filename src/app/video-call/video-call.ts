9ab8418de4afc426ad7c482785e343d2cdde8906
Yukselcsgn <yukselcosgun@hotmail.com.tr>
2026-01-14 11:12:32 +0300

    commit

---
+++ src/app/video-call/video-call.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-call',
+  imports: [],
+  templateUrl: './video-call.html',
+  styleUrl: './video-call.css',
+})
+export class VideoCall {
+
+}
